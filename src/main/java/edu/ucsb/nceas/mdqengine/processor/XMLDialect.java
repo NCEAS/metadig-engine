dd9257ab524b58edcc2e55a44df53a8094146a20
leinfelder <leinfelder@nceas.ucsb.edu>
2016-06-15 09:23:55 -0700

    handle single and multiple values returned from xpath expressions

--- src/main/java/edu/ucsb/nceas/mdqengine/processor/XMLDialect.java
+++ src/main/java/edu/ucsb/nceas/mdqengine/processor/XMLDialect.java
@@ -2,8 +2,10 @@ package edu.ucsb.nceas.mdqengine.processor;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.script.ScriptException;
@@ -11,10 +13,15 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import edu.ucsb.nceas.mdqengine.dispatch.Dispatcher;
@@ -25,8 +32,10 @@ import edu.ucsb.nceas.mdqengine.model.Selector;
 public class XMLDialect {
 	
 	private Document document;
+	
 	private XPathFactory xPathfactory;
-
+	
+	protected Log log = LogFactory.getLog(this.getClass());
 	
 	public XMLDialect(InputStream input) throws SAXException, IOException, ParserConfigurationException {
 		
@@ -43,10 +52,37 @@ public class XMLDialect {
 		// gather the required information
 		Map<String, Object> variables = new HashMap<String, Object>();
 		for (Selector selector: check.getSelectors()) {
+			
+			String name = selector.getName();
+			Object value = null;
+			
+			// select one or more values from document
 			String selectorPath = selector.getXpath();
 			XPath xpath = xPathfactory.newXPath();
-			String value = xpath.evaluate(selectorPath, document);
-			String name = selector.getName();
+			
+			// try multiple first
+			NodeList nodes = null;
+			try {
+				nodes = (NodeList) xpath.evaluate(selectorPath, document, XPathConstants.NODESET);
+				if (nodes.getLength() > 1) {
+					// multiple values
+					List<String> values = new ArrayList<String>();
+					for (int i = 0; i < nodes.getLength(); i++) {
+						Node node = nodes.item(i);
+						values.add(node.getTextContent());
+					}
+					value = values;
+				} else {
+					// single value
+					value = nodes.item(0).getTextContent();
+				}
+			} catch (XPathExpressionException xpee) {
+				log.warn("Defaulting to single value selection: " + xpee.getCause().getMessage());
+				// try just a single value
+				value = xpath.evaluate(selectorPath, document);
+			}
+			
+			// make available in script
 			variables.put(name, value);
 		}
 		
