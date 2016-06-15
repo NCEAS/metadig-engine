37d4b1682a3bbb45829467df13fcea5946712e5a
leinfelder <leinfelder@nceas.ucsb.edu>
2016-06-15 10:45:50 -0700

    add JSON serialization for our simple object model as a first pass.

---
+++ src/main/java/edu/ucsb/nceas/mdqengine/serialize/JsonMarshaller.java
@@ -0,0 +1,16 @@
+package edu.ucsb.nceas.mdqengine.serialize;
+
+import com.google.gson.Gson;
+
+public class JsonMarshaller {
+
+	private static Gson gson = new Gson();
+
+	public static String toJson(Object obj) {
+		return gson.toJson(obj);
+	}
+	
+	public static Object fromJson(String json, Class<Object> clazz) {
+		return gson.fromJson(json, clazz);
+	}
+}
