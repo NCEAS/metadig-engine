ef4f49cf07d66aff898dd8ed38b6909c4ae9faab
leinfelder <leinfelder@nceas.ucsb.edu>
2016-06-22 06:17:39 -0700

    add check.description to model to align with existing LTER QC checks.

--- src/main/java/edu/ucsb/nceas/mdqengine/model/Check.java
+++ src/main/java/edu/ucsb/nceas/mdqengine/model/Check.java
@@ -13,6 +13,8 @@ public class Check {
 
 	private String name;
 	
+	private String description;
+
 	private String type;
 	
 	private String level;
@@ -40,6 +42,14 @@ public class Check {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getType() {
