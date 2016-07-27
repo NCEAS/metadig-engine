47e71f940adb0586262ee95cfdfb7c4914dfd22f
Bryce Mecum <petridish@gmail.com>
2016-07-27 12:38:13 -0800

    Facet the result plot conditionally

--- src/main/resources/code/plot.R
+++ src/main/resources/code/plot.R
@@ -16,15 +16,21 @@ results_summarized <- results %>%
             datasource = unique(datasource))
 
 
-# Plot
-png(outputPath, width=400, height=400)
-ggplot(results_summarized, aes(level, proportion, fill = status)) +
+# Create the plot
+g <- ggplot(results_summarized, aes(level, proportion, fill = status)) +
   geom_bar(stat = "identity") +
   scale_fill_manual(drop = FALSE,
                     values = c("#00CC00", "#CC0000", "#CCCCCC", "#CCCC00")) +
-  facet_wrap(~datasource) +
   labs(x = "", y = "Proportion of All Checks Run") +
   theme(legend.title = element_blank())
+
+if ("datasource" %in% names(results) && (length(unique(results$datasource))> 1)) {
+  g <- g + facet_wrap(~datasource)
+}
+
+# Save to disk
+png(outputPath, width=400, height=400)
+g
 dev.off()
 
 # Pass on result
