b0cc461f5e72f9c9484a1ea16e48984d3e3a086a
Bryce Mecum <petridish@gmail.com>
2016-07-27 11:47:20 -0800

    Improve the result plot to specify colors and use proportions
    - Plot uses the classic ggplot theme and no legend title
    - Fill colors now use a specific set of colors
    - Heights on bars and yscale are now in proportions of all checks

--- src/main/resources/code/plot.R
+++ src/main/resources/code/plot.R
@@ -1,15 +1,31 @@
 library(methods)
 library(svglite)
 library(ggplot2)
+theme_set(theme_classic())
+library(dplyr)
 
-#args = commandArgs(trailingOnly=TRUE)
-#inputPath = args[1]
-#outputPath = args[2]
-
+# Load + munge
 results = read.csv(inputPath)
+results$status <- factor(results$status,
+                         levels = c("SUCCESS", "FAILURE", "SKIP", "ERROR"),
+                         ordered = TRUE)
+results_summarized <- results %>%
+  mutate(nchecks = length(pid)) %>%
+  group_by(status, level) %>%
+  summarize(proportion = length(status) / unique(nchecks),
+            datasource = unique(datasource))
 
+
+# Plot
 png(outputPath, width=400, height=400)
-ggplot(results, aes(level, fill = status)) + geom_bar() + facet_wrap(~ datasource)
+ggplot(results_summarized, aes(level, proportion, fill = status)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(drop = FALSE,
+                    values = c("#00CC00", "#CC0000", "#CCCCCC", "#CCCC00")) +
+  facet_wrap(~datasource) +
+  labs(x = "", y = "Proportion of All Checks Run") +
+  theme(legend.title = element_blank())
 dev.off()
 
+# Pass on result
 result = list(value = outputPath, status="SUCCESS", message="See output path for plot." );
