648ec66da0edb041c9c6084cd9b3b9643ed36106
Bryce Mecum <petridish@gmail.com>
2016-08-05 11:10:53 -0800

    Extend plot.R code to plot single and multi-doc runs

--- src/main/resources/code/plot.R
+++ src/main/resources/code/plot.R
@@ -4,7 +4,95 @@ library(ggplot2)
 theme_set(theme_classic())
 library(dplyr)
 
-# Load + munge
+
+#' Create a ggplot plot for a result on a single metadata doc
+#'
+#' @param x (data.frame) An MDQ results data frame
+#'
+#' @return (gg) The ggplot2 plot object
+#' @export
+#'
+#' @examples
+makeSingleDocPlot <- function(x) {
+  results_summarized <- x %>%
+    mutate(denom = length(pid)) %>%
+    group_by(pid, type, status, level) %>%
+    summarize(proportion = length(status) / unique(denom)[1])
+
+
+  g <- ggplot(results_summarized, aes(level, proportion, fill = status)) +
+    geom_bar(stat = "identity") +
+    scale_fill_manual(drop = FALSE,
+                      values = c("#d9edf7", "#f2dede", "#f5f5f5", "#fcf8e3")) +
+    facet_wrap(~ type) +
+    labs(x = "", y = "Proportion of All Checks Run") +
+    theme(legend.title = element_blank(),
+          panel.border = element_rect(colour = "black", fill = NA),
+          legend.position = "top")
+
+  g
+}
+
+#' Create a ggplot plot for a result on multiple metadata documents
+#'
+#' @param x (data.frame) An MDQ results data frame
+#'
+#' @return (gg) The ggplot2 plot object
+#' @export
+#'
+#' @examples
+makeMultiDocPlot <- function(x) {
+  results_summarized <- x %>%
+    mutate(denom = length(pid)) %>%
+    group_by(datasource, status, level) %>%
+    summarize(proportion = length(status) / unique(denom)[1])
+
+
+  g <- ggplot(results_summarized, aes(level, proportion, fill = status)) +
+    geom_bar(stat = "identity") +
+    scale_fill_manual(drop = FALSE,
+                      values = c("#d9edf7", "#f2dede", "#f5f5f5", "#fcf8e3"))  +
+    labs(x = "", y = "Proportion of All Checks Run") +
+    theme(legend.title = element_blank(),
+          panel.border = element_rect(colour = "black", fill = NA),
+          legend.position = "top")
+
+  if ("datasource" %in% names(results_summarized) && (length(unique(results_summarized$datasource)) > 1)) {
+    g <- g + facet_wrap(~datasource)
+  }
+
+  g
+}
+
+#' Wrapper function that creates result plots for single and multiple document
+#' results.
+#'
+#' Note: The plot is saved as a PNG at `path`.
+#'
+#' @param x (data.frame) An MDQ results data frame
+#' @param path (character) The path to save the plot
+#'
+#' @return (character) The path where the plot was saved
+#' @export
+#'
+#' @examples
+makeMDQPlot <- function(x, path) {
+  if (length(unique(x$pid)) == 1) {
+    g <- makeSingleDocPlot(x)
+  } else if ("datasource" %in% names(x) && (length(unique(x$datasource)) > 1)) {
+    g <- makeMultiDocPlot(x)
+  }
+
+  ggsave(filename = path,
+         plot = g,
+         width = 4,
+         height = 5)
+
+  path
+}
+
+
+# Load + munge data
 results = read.csv(inputPath)
 
 results$type <- factor(results$type,
@@ -19,30 +107,10 @@ results$status <- factor(results$status,
                          levels = c("SUCCESS", "FAILURE", "SKIP", "ERROR"),
                          ordered = TRUE)
 
-results_summarized <- results %>%
-  mutate(denom = length(pid)) %>%
-  group_by(datasource, status, level) %>%
-  summarize(proportion = length(status) / unique(denom)[1])
 
-# Create the plot
-g <- ggplot(results_summarized, aes(level, proportion, fill = status)) +
-  geom_bar(stat = "identity") +
-  scale_fill_manual(drop = FALSE,
-                    values = c("#d9edf7", "#f2dede", "#f5f5f5", "#fcf8e3"))  +
-  labs(x = "", y = "Proportion of All Checks Run") +
-  theme(legend.title = element_blank(),
-        panel.border = element_rect(colour = "black", fill = NA),
-        legend.position = "top")
-
-if ("datasource" %in% names(results_summarized) && (length(unique(results_summarized$datasource)) > 1)) {
-  g <- g + facet_wrap(~datasource)
-}
-
-# Save to disk
-ggsave(filename = outputPath,
-       plot = g,
-       width = 4,
-       height = 5)
+# Make the plot
+outputPath <- makeMDQPlot(results, outputPath)
 
 # Pass on result
-mdq_result = list(status="SUCCESS", list(output=list(value = outputPath)  ) );
+mdq_result = list(status = "SUCCESS",
+                  output = list(list(value = outputPath)))
