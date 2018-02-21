1d197d2f4333033001f770ed9ad0abc86af3b607
gothub <slaughter@nceas.ucsb.edu>
2018-02-21 15:41:14 -0800

    Use newest jar for command line tests

--- bin/runWorker.sh
+++ bin/runWorker.sh
@@ -2,4 +2,4 @@
 
 #mvn exec:java -Dexec.mainClass="edu.ucsb.nceas.metadig.Worker" -Dexec.args="hi there"
 
-java -cp ./target/mdqengine-1.1.2-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Worker
+java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Worker
