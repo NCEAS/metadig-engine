8d86d3c578855544baeac2bfdd18986e31bcc2c4
gothub <slaughter@nceas.ucsb.edu>
2018-05-25 14:41:47 -0700

    Move ./nginx to ./Admin/metadig-nginx

---
+++ Kubernetes/Admin/metadig-nginx/download.sh
@@ -0,0 +1,7 @@
+# Already ran this, make sure it doesn't get run again
+#curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/default-backend.yaml > default-backend.yaml
+#curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/configmap.yaml > configmap.yaml
+#curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/tcp-services-configmap.yaml > tcp-services-configmap.yaml
+#curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/udp-services-configmap.yaml > udp-services-configmap.yaml
+#curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/with-rbac.yaml  > with-rbac.yaml
+
