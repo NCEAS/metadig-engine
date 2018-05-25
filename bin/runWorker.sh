d7cec471b457170bf4c8de3b47bb9636c99af37c
gothub <slaughter@nceas.ucsb.edu>
2018-05-25 14:51:31 -0700

    Add needed D1 indexing files to CLASSPATH

--- bin/runWorker.sh
+++ bin/runWorker.sh
@@ -2,4 +2,4 @@
 
 #mvn exec:java -Dexec.mainClass="edu.ucsb.nceas.metadig.Worker" -Dexec.args="hi there"
 
-java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Worker
+java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar:./target/classes/solr edu.ucsb.nceas.mdqengine.Worker
