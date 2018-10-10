5e152a009384c39af75bb1226a595f72c98d6e8d
gothub <slaughter@nceas.ucsb.edu>
2018-10-10 15:12:10 -0700

    Allow args to controller to enable testing

--- bin/runController.sh
+++ bin/runController.sh
@@ -2,4 +2,4 @@
 
 #mvn exec:java -Dexec.mainClass="edu.ucsb.nceas.metadig.Controller" -Dexec.args="hi there"
 
-java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Controller
+java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Controller $*
