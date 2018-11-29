4fda249889b45f994fd026c305d7a8cec83022a2
gothub <slaughter@nceas.ucsb.edu>
2018-11-29 09:42:37 -0800

    Update scripts for development testing

--- bin/runController.sh
+++ bin/runController.sh
@@ -2,4 +2,7 @@
 
 #mvn exec:java -Dexec.mainClass="edu.ucsb.nceas.metadig.Controller" -Dexec.args="hi there"
 
-java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Controller $*
+# Use first argument if sending jobs via port, for testing, for example:
+# java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Controller 3300*
+java -cp ./target/metadig-engine-2.0.0-RC1.jar edu.ucsb.nceas.mdqengine.Controller $*
+
--- bin/runWorker.sh
+++ bin/runWorker.sh
@@ -2,4 +2,4 @@
 
 #mvn exec:java -Dexec.mainClass="edu.ucsb.nceas.metadig.Worker" -Dexec.args="hi there"
 
-java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar:./target/classes/solr edu.ucsb.nceas.mdqengine.Worker
+java -cp ./target/metadig-engine-2.0.0-RC1.jar:./target/classes/solr edu.ucsb.nceas.mdqengine.Worker
