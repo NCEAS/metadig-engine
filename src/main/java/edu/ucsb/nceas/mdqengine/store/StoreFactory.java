2f9eb25f06a4a7bc956fa15a5c1d8fd9cc5a81d3
gothub <slaughter@nceas.ucsb.edu>
2019-04-22 11:29:58 -0700

    Resolve problem of stale db connections failing

--- src/main/java/edu/ucsb/nceas/mdqengine/store/StoreFactory.java
+++ src/main/java/edu/ucsb/nceas/mdqengine/store/StoreFactory.java
@@ -1,32 +1,29 @@
 package edu.ucsb.nceas.mdqengine.store;
 
-import edu.ucsb.nceas.mdqengine.exception.MetadigException;
 import edu.ucsb.nceas.mdqengine.exception.MetadigStoreException;
-import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import java.io.IOException;
 
 public class StoreFactory {
 
     private static Log log = LogFactory.getLog(StoreFactory.class);
 
-    private static MDQStore store = null;
-
-    public static MDQStore getStore(boolean persist) throws MetadigException, IOException, ConfigurationException {
-        if (store == null) {
-            if (persist) {
-                log.debug("Creating new MDQ persistent store");
-                try {
-                    store = new DatabaseStore();
-                } catch (MetadigStoreException e) {
-                    e.printStackTrace();
-                    throw(e);
-                }
-            } else {
-                log.debug("Creating new MDQ store");
+    public static MDQStore getStore(boolean persist) throws MetadigStoreException {
+        MDQStore store = null;
+        if (persist) {
+            log.debug("Creating new MDQ persistent store");
+            try {
+                store = new DatabaseStore();
+            } catch (MetadigStoreException e) {
+                e.printStackTrace();
+                throw(e);
+            }
+        } else {
+            log.debug("Creating new MDQ store");
+            try {
                 store = new InMemoryStore();
+            } catch (MetadigStoreException mse) {
+                throw new MetadigStoreException(mse.getMessage());
             }
         }
         return store;
