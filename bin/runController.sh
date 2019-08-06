1f3c825572983c1e6b9240828fa13576f8ab23ca
gothub <slaughter@nceas.ucsb.edu>
2019-08-06 13:26:36 -0700

    Update version in dev testing scripts

--- bin/runController.sh
+++ bin/runController.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+version=2.1.0
 #mvn exec:java -Dexec.mainClass="edu.ucsb.nceas.metadig.Controller" -Dexec.args="hi there"
 
 # Use first argument if sending jobs via port, for testing, for example:
 # java -cp ./target/metadig-engine-1.1.3-SNAPSHOT.jar edu.ucsb.nceas.mdqengine.Controller 3300*
-java -cp ./target/metadig-engine-2.0.0-RC1.jar edu.ucsb.nceas.mdqengine.Controller $*
+java -cp ./target/metadig-engine-${version}.jar edu.ucsb.nceas.mdqengine.Controller $*
 
