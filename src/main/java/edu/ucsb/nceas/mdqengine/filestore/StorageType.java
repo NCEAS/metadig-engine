388d7abf57cdd42abee70d0483f4a539235c38c2
gothub <slaughter@nceas.ucsb.edu>
2019-11-01 16:15:31 -0700

    Improvements to filestore storage type management

--- src/main/java/edu/ucsb/nceas/mdqengine/filestore/StorageType.java
+++ src/main/java/edu/ucsb/nceas/mdqengine/filestore/StorageType.java
@@ -13,6 +13,37 @@ public enum StorageType {
         this.type = type;
     }
 
+    public String toString(){
+        switch(this){
+            case DATA:
+                return "data";
+            case GRAPH:
+                return "graph";
+            case METADATA:
+                return "metadata";
+            case CODE:
+                return "code";
+            case TMP:
+                return "tmp";
+        }
+        return null;
+    }
+
+    public static StorageType getValue(String value) {
+        if(value.equalsIgnoreCase(DATA.toString()))
+            return StorageType.DATA;
+        else if(value.equalsIgnoreCase(GRAPH.toString()))
+            return StorageType.GRAPH;
+        else if(value.equalsIgnoreCase(METADATA.toString()))
+            return StorageType.METADATA;
+        else if(value.equalsIgnoreCase(CODE.toString()))
+            return StorageType.CODE;
+        else if(value.equalsIgnoreCase(TMP.toString()))
+            return StorageType.TMP;
+        else
+            return null;
+    }
+
     public String getStorageTyp() {
         return this.type.toLowerCase();
     }
