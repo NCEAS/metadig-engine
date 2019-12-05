9053838e5bfeb2a8f434bfebac359a71c3358718
gothub <slaughter@nceas.ucsb.edu>
2019-12-04 19:54:10 -0800

    Ensure x-axis is always rendered

--- src/main/resources/code/graph_cumulative_quality_scores.R
+++ src/main/resources/code/graph_cumulative_quality_scores.R
@@ -24,10 +24,16 @@ scores <- mutate(fsr, ym = as.Date(sprintf("%4s-%02d-01", year(dateUploaded), mo
   mutate(scoreI = scoreInteroperable * 100.0) %>%
   mutate(scoreR = scoreReusable * 100.0)
 
+# Use this when sequenceId problem has been resolved (github metadig-engine #232)
+#most_recent <- scores %>%
+#  arrange(ym, sequenceId, dateUploaded) %>%
+#  group_by(ym, sequenceId) %>%
+#  top_n(1, dateUploaded)
+#head(most_recent)
+
 most_recent <- scores %>%
-  arrange(ym, sequenceId, dateUploaded) %>%
-  group_by(ym, sequenceId) %>%
-  top_n(1, dateUploaded)
+  arrange(ym) %>%
+  group_by(ym)
 head(most_recent)
 
 # calculate cummulative overall
@@ -45,6 +51,21 @@ score_cumulative$metric <- factor(score_cumulative$metric,
 score_monthly <- score_cumulative %>% filter(metric %in% c("Findable", "Accessible", "Interoperable", "Reusable"))
 score_cumulative_alone <- score_cumulative %>% filter(metric %in% c("Cum. Findable", "Cum. Accessible", "Cum. Interoperable", "Cum. Reusable"))
 
+# See if the 'dateUploaded' dates span multiple years and if not, the x-axis needs to be configured for ggplot so that
+# it will display. If it is configured for years and only a single year exists, the x-axis will not display.
+minYear <- format(with(scores, min(dateUploaded)), "%Y")
+maxYear <- format(with(scores, max(dateUploaded)), "%Y")
+if(minYear == maxYear) {
+  xLabel <- "Month"
+  dateBreaks <- "months"
+  dateMinorBreaks <- "day"
+  dateFormat <- "%Y-%m"
+} else {
+  xLabel <- "Year"
+  dateBreaks <- "year"
+  dateMinorBreaks <- "months"
+  dateFormat <- "%Y"
+}
 
 # Plot cummulative overall
 d1_colors <- c("#ff582d", "#c70a61", "#1a6379", "#60c5e4", "#ff582d", "#c70a61", "#1a6379", "#60c5e4")
@@ -57,15 +78,13 @@ p <- ggplot(data=score_cumulative_alone, mapping=aes(x=ym, y=mean, color=metric)
         panel.grid.minor = element_blank(),
         panel.background = element_blank()) +
   scale_colour_manual(values=d1_colors) +
-  scale_x_date(date_breaks="year", date_minor_breaks="months", labels=date_format("%Y")) +
-  xlab("Year") +
+  scale_x_date(date_breaks=dateBreaks, date_minor_breaks=dateMinorBreaks, labels=date_format(dateFormat)) +
+  xlab(xLabel) +
   scale_y_continuous(limits=c(0,100)) +
   ylab("Average FAIR Score") +
   #ggtitle(paste0("DataONE: FAIR scores for ", format(sum(standards$n), big.mark=","), " EML and ISO metadata records"))
-  #ggtitle(title)
-ggsave(outFile, width = 8, height = 4)
+ggsave(outFile, width = 8, height = 3)
 
-#output <- "/Users/slaughter/tmp/dataone-fair-scores-overall.png"
 output <- sprintf("Created graphics file %s", outFile)
 status <- "SUCCESS"
 
