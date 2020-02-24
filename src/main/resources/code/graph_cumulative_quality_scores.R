1bfd93c0f04a3abf525e113cd704bad1b5e5b5c8
gothub <slaughter@nceas.ucsb.edu>
2020-02-24 12:06:16 -0800

    Axis label changes to quality charts

--- src/main/resources/code/graph_cumulative_quality_scores.R
+++ src/main/resources/code/graph_cumulative_quality_scores.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(scales)
 library(lubridate)
 library(readr)
+library(magrittr)
 
 # Plot cummulative quality scores by month
 # This program is dispatched (called) by the MetaDIG Grapher class. Several
@@ -14,6 +15,18 @@ library(readr)
 # - outFile: the graphics output file to create
 # Variables read by metadig-engine Dispatcher after execution
 # mdq_result, output, status 
+
+# Define these variable for local testing only
+#inFile <- "dbo.csv"
+#outFile <- "dbo.png"
+#inFile <- "sasap.csv"
+#outFile <- "sasap.png"
+#inFile <- "FAIR-scores-eml.csv"
+#outFile <- "FAIR-scores-eml.png"
+axisTextFontSize <- 7
+legendTextFontSize <- 7
+axisTitleFontSize <- 9
+legendTitleFontSize <- 9
 
 # Load data
 fsr <- read_csv(inFile)
@@ -49,7 +62,11 @@ score_cumulative$metric <- factor(score_cumulative$metric,
                                   labels=c("Findable", "Accessible", "Interoperable", "Reusable",
                                            "Cum. Findable", "Cum. Accessible", "Cum. Interoperable", "Cum. Reusable"))
 score_monthly <- score_cumulative %>% filter(metric %in% c("Findable", "Accessible", "Interoperable", "Reusable"))
-score_cumulative_alone <- score_cumulative %>% filter(metric %in% c("Cum. Findable", "Cum. Accessible", "Cum. Interoperable", "Cum. Reusable"))
+# Calculate the overall mean for each FAIR category
+mf <- score_cumulative %>% filter(metric %in% c("Findable")) %>% extract2("mean") %>% mean(., na.rm = TRUE)
+ma <- score_cumulative %>% filter(metric %in% c("Accessible")) %>% extract2("mean") %>% mean(., na.rm = TRUE)
+mi <- score_cumulative %>% filter(metric %in% c("Interoperable")) %>% extract2("mean") %>% mean(., na.rm = TRUE)
+mr <- score_cumulative %>% filter(metric %in% c("Reusable")) %>% extract2("mean") %>% mean(., na.rm = TRUE)
 
 # See if the 'dateUploaded' dates span multiple years and if not, the x-axis needs to be configured for ggplot so that
 # it will display. If it is configured for years and only a single year exists, the x-axis will not display.
@@ -69,21 +86,31 @@ if(minYear == maxYear) {
 
 # Plot cummulative overall
 d1_colors <- c("#ff582d", "#c70a61", "#1a6379", "#60c5e4", "#ff582d", "#c70a61", "#1a6379", "#60c5e4")
-p <- ggplot(data=score_cumulative_alone, mapping=aes(x=ym, y=mean, color=metric)) +
+p <- ggplot(data=score_monthly, mapping=aes(x=ym, y=mean, color=metric)) +
   geom_line() +
   geom_point(size=1) +
   theme_bw() +
   theme(panel.border = element_blank(),
         axis.line = element_line(colour = "black"),
+        axis.text = element_text(size = axisTextFontSize),
+        axis.title = element_text(size = axisTitleFontSize),
+        legend.title = element_text(size = legendTitleFontSize),
+        legend.text = element_text(size = legendTextFontSize),
         panel.grid.minor = element_blank(),
         panel.background = element_blank()) +
-  scale_colour_manual(values=d1_colors) +
+  #scale_color_manual(name = "Metric", labels = c("Findable",  "Accessible", "Interoperable", "Reusable"), 
+  #                   values=d1_colors) +
+  scale_color_manual(name = "Metric", labels = c(sprintf("Findable (%.0f%%)", mf), 
+                                sprintf("Accessible (%.0f%%)", ma),
+                                sprintf("Interoperable (%.0f%%)", mi),
+                                sprintf("Reusable (%.0f%%)", mr)), values=d1_colors) +
   scale_x_date(date_breaks=dateBreaks, date_minor_breaks=dateMinorBreaks, labels=date_format(dateFormat)) +
   xlab(xLabel) +
   scale_y_continuous(limits=c(0,100)) +
   ylab("Average FAIR Score") +
   #ggtitle(paste0("DataONE: FAIR scores for ", format(sum(standards$n), big.mark=","), " EML and ISO metadata records"))
-ggsave(outFile, width = 8, height = 3)
+  #scale_fill_discrete(name = "metric", labels = c("Finabl", "Accessibl", "Interoperabl", "Reusabl")) +
+  ggsave(outFile, width = 8, height = 3)
 
 output <- sprintf("Created graphics file %s", outFile)
 status <- "SUCCESS"
