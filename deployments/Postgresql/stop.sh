232043a5033b95c13d37d6f9a7ac50cc860c01ee
gothub <slaughter@nceas.ucsb.edu>
2022-05-18 18:35:17 -0700

    Remove obsolete 'deployment' directory
    Issue #152 #317 The YAML files used for the manual installation of metadig engine has been replace by helm.

--- deployments/Postgresql/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f postgresql.yaml
--- deployments/metadig-engine/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f metadig-worker.yaml
-kubectl delete -f metadig-controller.yaml
-kubectl delete -f metadig-scheduler.yaml
-kubectl delete -f rabbitmq.yaml
