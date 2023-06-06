1d61c55805105ee78261ab9ad41f6dcff691cab5
Jeanette Clark <jclark@nceas.ucsb.edu>
2023-06-06 14:24:13 -0700

    update version number in all the relvant files

--- bin/startScheduler.sh
+++ bin/startScheduler.sh
@@ -2,6 +2,6 @@
 
 # Start a local copy of metadig-scheduler for RabbitMQ based development testing.
 
-${JAVA_HOME}/bin/java -cp /opt/local/metadig/config:./target/metadig-engine-2.4.0.jar edu.ucsb.nceas.mdqengine.scheduler.JobScheduler
+${JAVA_HOME}/bin/java -cp /opt/local/metadig/config:./target/metadig-engine-2.4.1.jar edu.ucsb.nceas.mdqengine.scheduler.JobScheduler
 
 
