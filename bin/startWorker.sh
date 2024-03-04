2b52e6ec05338499d3b35587cfb4201220d625f1
Jeanette Clark <jclark@nceas.ucsb.edu>
2024-03-04 13:41:08 -0700

    remove commented out code

--- bin/startWorker.sh
+++ bin/startWorker.sh
@@ -3,8 +3,5 @@
 # Start a local copy of metadig-worker for RabbitMQ based development testing.
 version=3.0.0-SNAPSHOT
 
-# Include the MetaDIG python library
-#export JYTHONPATH=/opt/local/metadig/metadig-py
-
 java -cp /opt/local/metadig/config:./target/metadig-engine-${version}.jar:./target/classes/solr edu.ucsb.nceas.mdqengine.Worker
 
