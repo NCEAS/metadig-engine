0d348cf28984cff64d7688dd71aa541c5453b8fc
Jeanette Clark <jclark@nceas.ucsb.edu>
2025-05-08 15:19:03 -0700

    add documentation

--- src/main/java/edu/ucsb/nceas/mdqengine/model/Expression.java
+++ src/main/java/edu/ucsb/nceas/mdqengine/model/Expression.java
@@ -4,9 +4,11 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 
 /**
- * The more general expression optionally replaces xpath, with an attribute
- * syntax giving the syntax of the expression (default is xpath, could also be
- * json-path).
+ * The expression field contains some kind of expression that enables extraction
+ * of a particular piece of metadata from the overall document. This more
+ * general expression optionally replaces the xpath element. It contains a
+ * syntax attribute which gives the syntax of the expression (default is xpath,
+ * could also be json-path if looking in json documents as opposed to xml).
  */
 public class Expression {
 
@@ -16,19 +18,49 @@ public class Expression {
     @XmlValue
     private String value;
 
+    /**
+     * Returns the syntax used to interpret the expression.
+     *
+     * This value is mapped from the syntax XML attribute.
+     * If not explicitly set, it defaults to "xpath".
+     *
+     * @return the syntax string (e.g., "xpath", "json-path", etc.)
+     */
     public String getSyntax() {
         return syntax;
     }
 
+    /**
+     * Sets the syntax used to interpret the expression.
+     *
+     * This value corresponds to the {@code syntax} XML attribute.
+     *
+     * @param syntax the syntax string to set (e.g., "xpath", "json-path".)
+     */
     public void setSyntax(String syntax) {
         this.syntax = syntax;
     }
 
+    /**
+     * Returns the expression value.
+     *
+     * This is the text content of the XML element.
+     *
+     * @return the expression string
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Sets the expression value.
+     *
+     * This sets the text content of the XML element.
+     *
+     * @param value the expression string to set
+     */
     public void setValue(String value) {
         this.value = value;
     }
+
 }
