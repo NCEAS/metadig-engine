2ad4bf6f467ade251cf62a3d1aac262b64760068
Jeanette Clark <jclark@nceas.ucsb.edu>
2025-07-16 13:15:18 -0700

    install solr and rabbitmq via chart

--- helm/metadig-rabbitmq/install-metadig-rabbitmq.sh
+++ helm/metadig-rabbitmq/install-metadig-rabbitmq.sh
@@ -3,28 +3,8 @@
 # https://github.com/bitnami/charts/tree/master/bitnami/rabbitmq#installing-the-chart
 
 # helm repo add bitnami https://charts.bitnami.com/bitnami
-
 helm upgrade metadig-rabbitmq bitnami/rabbitmq \
---version=12.8.2 \
---namespace metadig \
---set image.registry=docker.io \
---set image.repository=bitnami/rabbitmq \
---set auth.username=metadig \
---set auth.password=quality \
---set replicaCount=3 \
---set podSecurityContext.enabled=false \
---set podSecurityContext.fsGroup=1001 \
---set podManagementPolicy=Parallel \
---set service.type=ClusterIP \
---set service.externalTrafficPolicy=Cluster \
---set ingress.enabled=false \
---set ingress.tls=false \
---set ingress.ingressClassName="nginx" \
---set persistence.enabled=true \
---set persistence.storageClass=csi-rbd-sc \
---set persistence.size=10Gi \
---set volumePermissions.enabled=false \
---set volumePermissions.containerSecurityContext.runAsUser=1001 \
---set serviceAccount.create=false \
---set serviceAccount.name="metadig" \
---set tls.enabled=false
+  --version=12.8.2 \
+  --namespace metadig \
+  -f rabbitmq-values.yaml
+
